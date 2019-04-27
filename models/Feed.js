ff97a9e53d7fd9cdbe04e283b4321c79eda04fee
Song <yousonghou@yahoo.com>
2019-04-27 19:59:26 +0000

    add feed model

---
+++ models/Feed.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const feedSchema = new Schema({
+    favoriteSport: {
+        type: String,
+        require: true
+    },
+    level: {
+        type: String,
+        require: true
+    },
+    city: {
+        type: String,
+        require: true
+    },
+    goal: {
+        type: String,
+        require: true
+    }
+});
+
+module.exports = mongoose.model('Feed', feedSchema); 
