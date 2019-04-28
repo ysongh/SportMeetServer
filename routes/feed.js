792e8f18f86a22abdd23a5bc6f31c9144ff76051
Song <yousonghou@yahoo.com>
2019-04-28 04:03:34 +0000

    applying passport middleware to post feed route

--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const passport = require('passport');
 
 const feedController = require("../controllers/feed");
 
 router.get('/', feedController.getAllFeed);
 
-router.post('/', feedController.addFeed);
+router.post('/', passport.authenticate('jwt', {session: false}), feedController.addFeed);
 
 router.get('/filter', feedController.filterFeed);
 
